cba1d84ef39f360bf3fc7988ab75d8639cb4d748
ElderSam <eldersamuel98@gmail.com>
2025-06-19 19:32:25 -0300

    fix: improve most frequent words (ignore stopwords & format text)

--- index.js
+++ index.js
@@ -16,29 +16,40 @@ app.post("/analyze-text", async (request, reply) => {
     const { text } = request.body;
     console.log({ text });
 
+    const stopwords = [
+        // Portuguese stopwords
+        "a", "o", "e", "de", "do", "da", "em", "um", "uma", "para", "com", "não", "que", "se", "na", "no", "os", "as", "por", "mais", "mas", "como", "ou", "dos", "das", "ao", "à", "às", "é", "foi", "ser", "tem", "há", "são",
+        // English stopwords
+        "the", "and", "is", "in", "it", "you", "that", "he", "was", "for", "on", "are", "with", "as", "I", "his", "they", "be", "at", "one", "have", "this", "from", "or", "had", "by", "not", "word", "but", "what", "some", "we", "can", "out", "other", "were", "all", "there", "when", "up", "use", "your", "how", "said", "an", "each", "she"
+    ];
+
     const words = text
-        .trim().split(/\s+/)
-        .filter(Boolean);
-
-    const wordsCount = words.length;
-
+        .toLowerCase()
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // remove accents
+        .split(/\s+/)
+        .map(w => w.replace(/[^\w]/g, "")) // remove ponctuaction
+        .filter(Boolean)
+        .filter(w => !stopwords.includes(w));
 
     const freq = {};
 
     // words frequency
-    for(const word of words) {
+    for (const word of words) {
         freq[word] = (freq[word] || 0) + 1;
     }
 
     // top 5 most frequently words
-    const mostFrequentWords = Object.entries(freq)
-        .sort((a, b) =>  b[1] - a[1]) // descending order
-        .slice(0, 5);
+    const topWords = Object.entries(freq)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 5)
+        .map(([word, count]) => ({ word, count }));
+
+    const wordsCount = words.length;
 
     const resData = {
         text,
         wordsCount,
-        mostFrequentWords
+        topWords
     };
 
     reply.send(resData);
