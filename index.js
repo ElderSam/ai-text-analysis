2853f1a09c2ebbf9f16bb57169d7d717433d5c62
ElderSam <eldersamuel98@gmail.com>
2025-06-19 17:55:16 -0300

    create server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", async (req, res) => {
+    return { message: "Hello, world!" };
+});
+
+const PORT = 3000;
+
+app.listen({ port: PORT }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server is running at ${address}`);
+});
